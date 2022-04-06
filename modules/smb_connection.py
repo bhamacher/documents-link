49a0d154d1211f0c9abb8229702d7097a48d1956
Deepak Kumar <d.kumar@zera.de>
2022-04-06 11:32:43 +0200

    add interface to connect to path.
    Signed-off-by: Deepak Kumar <d.kumar@zera.de>

--- modules/smb_connection.py
+++ modules/smb_connection.py
@@ -1,6 +1,7 @@
 from smb.SMBConnection import SMBConnection
 import socket
 from getpass import getpass
+from urllib.parse import urlparse
 
 class Singleton_smb_connection(type): # metaclass
     _instances = {}
@@ -12,20 +13,24 @@ class Singleton_smb_connection(type): # metaclass
         return cls._instances[cls]
 
 
-class SmbServerConnection(metaclass=Singleton_smb_connection):
+class SmbServerConnectionHandler(metaclass=Singleton_smb_connection):
     def __init__(self): # Initializing
         self.dict_conn = {}
 
-    def get_connection(self, server_name):
+    def get_connection(self, path_name):
+        url_parsed  = urlparse(path_name)
+        server_name = url_parsed.netloc
+        return self._get_connection(server_name)
+
+    def _get_connection(self, server_name):
         if not server_name in self.dict_conn:
-            user_name           = getpass(prompt='Give your user_name: ')
-            password            = getpass(prompt='Give your password: ')
+            user_name           = getpass(prompt = 'Give your user_name: ')
+            password            = getpass(prompt = 'Give your password: ')
             server_IP           = socket.gethostbyname(server_name)
-            server_machine_name = server_name
-            conn                = SMBConnection(user_name, password, "", server_machine_name)
+            conn                = SMBConnection(user_name, password, "", server_name)
             assert conn.connect(server_IP, 139)
-            self.dict_conn[server_machine_name] = conn
-        return self.dict_conn
+            self.dict_conn[server_name] = conn
+        return self.dict_conn[server_name]
 
     def get_connection_count(self):
         return len(self.dict_conn)
