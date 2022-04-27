32085d92af08532b1cf6df90b5f4dffd9069f9ab
bhamacher <b.hamacher@zera.de>
2022-04-27 15:44:35 +0200

    Test update_filetable with db_spy
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- unittesting/document_links_unittest.py
+++ unittesting/document_links_unittest.py
@@ -2,6 +2,7 @@ import unittest
 import os
 import sys
 from document_links import document_links
+from unittesting.spy.db_spy import db_spy
 
 
 def test_link_data():
@@ -10,16 +11,32 @@ def test_link_data():
 
 class Test(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.db = db_spy()
+        self.path='/home/bhamacher/workspace/documents-link/test_folder/folder_test'
+        return super().setUp()
 
     def test_1_update_filetable(self):
-        file_path_object = document_links.DocumentLinks()
-        update_filetable = file_path_object.update_filetable(test_link_data())
+        file_path_object = document_links.DocumentLinks(self.db)
+        update_filetable = file_path_object.update_filetable([self.path])
         self.assertTrue(update_filetable)
+        expectedResult={'/home/bhamacher/workspace/documents-link/test_folder/folder_test/notfound/nichtexit.doc',
+        '/home/bhamacher/workspace/documents-link/test_folder/folder_test/folder12/text21a.txt',
+        '/home/bhamacher/workspace/documents-link/test_folder/folder_test/folder12/text21.txt',
+        '/home/bhamacher/workspace/documents-link/test_folder/folder_test/folder11/text11.txt',
+        '/home/bhamacher/workspace/documents-link/test_folder/folder_test/folder2/folder21', 
+        '/home/bhamacher/workspace/documents-link/test_folder/folder_test/folder11/text11a.txt',
+        '/home/bhamacher/workspace/documents-link/test_folder/folder_test/folder2/folder21/text21.txt',
+        '/home/bhamacher/workspace/documents-link/test_folder/folder_test/folder2/foo2.txt',
+        '/home/bhamacher/workspace/documents-link/test_folder/folder_test/folder2/folder22',
+        '/home/bhamacher/workspace/documents-link/test_folder/folder_test/folder2/folder22/text22.txt',
+        '/home/bhamacher/workspace/documents-link/test_folder/folder_test/folder1/foo1.txt'}
+        self.assertEqual(expectedResult,self.db.fileTable)
 
-    def test_2_clean_filetable(self):
-        file_path_object = document_links.DocumentLinks()
-        clean_filetable = file_path_object.clean_filetable("")
-        self.assertTrue(clean_filetable)
+    # def test_2_clean_filetable(self):
+    #     file_path_object = document_links.DocumentLinks(self.db)
+    #     clean_filetable = file_path_object.clean_filetable([""])
+    #     self.assertTrue(clean_filetable)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
